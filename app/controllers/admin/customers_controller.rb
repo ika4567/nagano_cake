1364042c4f214bbeba0c700e577257a5e501d54c
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-02-20 14:54:02 +0000

    Add Customers#update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,10 +12,20 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer.id)
   end
-  
+
+
   def full_name
     customers.last_name + customers.first_name
   end
-  
+
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+
 end
