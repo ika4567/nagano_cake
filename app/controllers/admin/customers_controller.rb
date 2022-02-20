509c5b5bf516140f85f681d6d5f2e5bc749abb8a
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-02-20 14:44:20 +0000

    Start CustomersView編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,21 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   def update
   end
+  
+  def full_name
+    customers.last_name + customers.first_name
+  end
+  
 end
