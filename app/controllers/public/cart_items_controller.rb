caea473ba29694b96aa641f6e4d53025a08b0968
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-02-28 13:22:51 +0000

    Update Cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -5,28 +5,36 @@ class Public::CartItemsController < ApplicationController
   end
 
   def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
-    CartItem.destroy
+    CartItem.destroy(params[:id])
     redirect_to cart_items_path
   end
 
   def destroy_all
-    CartItem.item_id.destroy_all
+    cart_items = current_customer.cart_items.all
+    cart_items.destroy_all
     redirect_to cart_items_path
   end
 
   def create
-    if Item.find_by(:name)
+    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
+      @add_amount = current_customer.cart_items.find_by(item_id: params[:item_id])
+      @add_amount.amount += params[:amount].to_i
+      @add_amount.update(amount: params[:amount])
+      redirect_to cart_items_path
     else
       @cart_item = CartItem.new(cart_item_params)
       @cart_item.customer_id = current_customer.id
       @cart_item.save
       redirect_to cart_items_path
     end
-    
+
   end
 
   private
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,19 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :item
   belongs_to :customer
-  
+
+  def get_image
+    unless self.item.image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    self.item.image
+  end
+
+  # 小計
+  def sub_total
+    item.with_tax_price * amount
+  end
+
 end
