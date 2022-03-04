3fc2278e87432809b4aedbb916cd0ee05236b40a
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-03-04 13:45:15 +0000

    Update Public_layout

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,8 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_customer!, except: [:index, :show]
+
   def index
-    @items = Item.all
+    @items = Item.page(params[:page])
   end
 
   def show
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,12 +3,12 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
 
-  def get_image
+  def get_image(width,height)
     unless self.item.image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    self.item.image
+    self.item.image.variant(resize_to_limit: [width, height]).processed
   end
 
   # 小計
